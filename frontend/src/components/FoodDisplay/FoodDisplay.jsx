755543cb6e1a6dfb3b67dace31944c997188fb19
Ayush Nepali <anepali818@gmail.com>
2024-09-08 13:13:56 +0545

    Homepage

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,9 @@ const FoodDisplay = ({category}) => {
         <h2>Top Dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
-                return <FoodItem key={index} id={item.id}  name={ item.name} price={ item.price} description={item.description} image={item.image}/>
+                 if(category==="All" || category === item.category){
+                   return <FoodItem key={index} id={item.id}  name={ item.name} price={ item.price} description={item.description} image={item.image}/>
+                 }
             })}
         </div>
     </div>
