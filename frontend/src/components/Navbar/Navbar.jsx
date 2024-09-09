ed3a2b1b6f1f00e44e5f63d11e731b24ab8d8524
Ayush Nepali <anepali818@gmail.com>
2024-09-09 13:27:22 +0545

    Added: Place Order Feature

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("menu");
+    const {getTotalCartAmount} = useContext(StoreContext);
     return (
         <div className='navbar'>
-            <img src={assets.logo} alt="" className="logo" />
+           <Link to='/'> <img src={assets.logo} alt="" className="logo" /> </Link>
             <ul className="navbar-menu">
                 <Link to ='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
                 <a href ='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
@@ -17,8 +19,8 @@ const Navbar = ({setShowLogin}) => {
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="" />
                 <div className="navbar-search-icon">
-                    <img src={assets.basket_icon} alt="" className="basket" />
-                    <div className="dot">
+                    <Link to='/cart'> <img src={assets.basket_icon} alt="" className="basket" /></Link>
+                    <div className={getTotalCartAmount()===0?"":"dot"}>
 
                     </div>
 
