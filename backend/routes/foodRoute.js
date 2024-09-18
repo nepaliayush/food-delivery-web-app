5b2df5bec65ae5242b89270c305731a083b71a48
Ayush Nepali <nepaliayush818@gmail.com>
2024-09-18 17:17:09 +0545

    Created Crud API

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addFood } from '../controllers/foodController.js'
+import { addFood, listFood, removeFood } from '../controllers/foodController.js'
 import multer from "multer"
 
 const foodRouter = express.Router();
@@ -8,15 +8,18 @@ const foodRouter = express.Router();
 
 
 
-const Storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination:"uploads",
     filename:(req,file,cb)=>{
-        return cb(null,'${Date.now()}${file.originalname')
+        return cb(null, `${Date.now()}${file.originalname}`);
+
     }
 })
 
 const upload = multer ({storage:storage})
 
 foodRouter.post("/add",upload.single("image"),addFood)
+foodRouter.get("/list",listFood)
+foodRouter.post("/remove",removeFood)
 
 export default foodRouter
--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -25,8 +25,6 @@ const LoginPopup = ({setShowLogin}) => {
         {currState==="Login"? <p>Create a new account ?<span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
         :  <p>Already have an account?<span onClick={()=>setCurrState("Login")} >Login here</span></p>}
 
-  
-    
     </form>
    </div>
   )
